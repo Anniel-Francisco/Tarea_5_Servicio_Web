20d2d017fe49bb25eac280565680062271b0f1f2
Anniel <annielfranciscoreyes@gmail.com>
2023-11-07 02:08:15 -0400

    cambios

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // Endpoint para obtener la lista de contactos
 app.get("/contactos", async (req, res) => {
